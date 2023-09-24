ca6f8ebf8dca89fe85ba5cca63926f90ce0e4a5e
dandyfp <dandyfp@sakutambah.id>
2023-09-24 23:07:03 +0700

    Transaction

--- lib/data/repositories/transaction_repository.dart
+++ lib/data/repositories/transaction_repository.dart
@@ -3,5 +3,5 @@ import 'package:flix_id/domain/entities/transaction.dart';
 
 abstract interface class TransactionRepository {
   Future<Result<Transaction>> cretateTransaction({required Transaction transaction});
-  Future<Result<Transaction>> getUserTransactions({required String uid});
+  Future<Result<List<Transaction>>> getUserTransactions({required String uid});
 }
