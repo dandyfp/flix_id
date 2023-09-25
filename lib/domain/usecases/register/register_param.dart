f40daf30392abc1c9ecfeb712bed8303cd03584b
dandyfp <dandyfp@sakutambah.id>
2023-09-25 22:17:13 +0700

    Usecase logout, register

---
+++ lib/domain/usecases/register/register_param.dart
@@ -0,0 +1,12 @@
+class RegisterParam {
+  String name;
+  String email;
+  String password;
+  String photoUrl;
+  RegisterParam({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.photoUrl,
+  });
+}
