e051bc317bc50db4d947e68ea2c5e7079bb98605
dandyfp <dandyfp@sakutambah.id>
2023-09-30 15:29:53 +0700

    Provider

---
+++ lib/presentation/providers/usecase/get_movie_list_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_movie_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getMoviListHash() => r'cf3f74c29bfe5e3bd872d793ec89a7235e69a866';
+
+/// See also [getMoviList].
+@ProviderFor(getMoviList)
+final getMoviListProvider = AutoDisposeProvider<GetMoviList>.internal(
+  getMoviList,
+  name: r'getMoviListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$getMoviListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GetMoviListRef = AutoDisposeProviderRef<GetMoviList>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
